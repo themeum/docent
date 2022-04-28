281a02f137fe5cff63f65c06c33d3cb3e0e2124d
kawshar <kawshar@joomshaper.com>
2022-04-29 01:03:20 +0600

    fixes

--- post-format/social-buttons.php
+++ post-format/social-buttons.php
@@ -9,7 +9,7 @@ if( has_post_thumbnail( get_the_ID() ) ){
 ?>
 <?php if(get_theme_mod('social-share-wrap', true)) { ?>
 	<div class="social-share-wrap">
-        <div class="share-text"><?php echo esc_attr__( 'Share: ', 'docent' ); ?></div>
+        <div class="share-text"><?php echo esc_html__( 'Share: ', 'docent' ); ?></div>
 		<ul>
 			<li>
 				<a href="#" data-type="facebook" data-url="<?php echo esc_url($docent_pro_permalink); ?>" data-title="<?php echo esc_attr($docent_pro_titleget); ?>" data-description="<?php echo esc_attr($docent_pro_titleget); ?>" data-media="<?php echo esc_url( $docent_pro_media_url ); ?>" class="prettySocial facebook"><i class="fab fa-facebook-f"></i></a>
